78831638c4d6eb6ce1aa9c0158f9d71403015359
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2023-11-09 19:40:54 +0700

    delete

--- app/routes/product-route.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express')
-const ProductController = require('../controllers/product-controller')
-
-const ProductRouter = Router()
-
-ProductRouter.get('/', ProductController.getAllProducts)
-ProductRouter.post('/', ProductController.postProduct)
-
-module.exports = ProductRouter
