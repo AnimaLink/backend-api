bf628cbf1880cc11eec19bbfcdea0e605576c968
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2023-11-16 17:45:02 +0700

    feat: make update forum validator

--- app/validators/forum-validator.js
+++ app/validators/forum-validator.js
@@ -10,6 +10,15 @@ const ForumValidator = {
 
     return schema.validate(payload)
   },
+  updateForum: (payload) => {
+    const schema = Joi.object({
+      name: Joi.string().required(),
+      price: Joi.string().required(),
+      description: Joi.string().required(),
+    }).options({ abortEarly: false })
+
+    return schema.validate(payload)
+  },
 }
 
 module.exports = ForumValidator
