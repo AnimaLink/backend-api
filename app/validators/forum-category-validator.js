38bbba587498d49cf2f75cd6402b767ea224e0f2
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2023-11-19 21:03:49 +0700

    feat: make validations

---
+++ app/validators/forum-category-validator.js
@@ -0,0 +1,20 @@
+const Joi = require('joi')
+
+const ForumCategoryValidator = {
+  createForumCategory: (payload) => {
+    const schema = Joi.object({
+      name: Joi.string().required(),
+    }).options({ abortEarly: false })
+
+    return schema.validate(payload)
+  },
+  updateForumCategory: (payload) => {
+    const schema = Joi.object({
+      name: Joi.string().required(),
+    }).options({ abortEarly: false })
+
+    return schema.validate(payload)
+  },
+}
+
+module.exports = ForumCategoryValidator
