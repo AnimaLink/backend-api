3e69c2069a0d8c9203d13995986ff9165e03cbb7
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2023-12-16 15:38:18 +0700

    add trim to string body

--- app/validators/user-validator.js
+++ app/validators/user-validator.js
@@ -3,9 +3,9 @@ const Joi = require('joi')
 const UserValidator = {
   updateUser: (payload) => {
     const schema = Joi.object({
-      first_name: Joi.string().required(),
-      last_name: Joi.string().required(),
-      wa_number: Joi.string().required(),
+      first_name: Joi.string().trim().required(),
+      last_name: Joi.string().trim().required(),
+      wa_number: Joi.string().trim().required(),
     }).options({ abortEarly: false })
 
     return schema.validate(payload)
