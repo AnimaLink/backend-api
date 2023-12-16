0dc02c54b4b5ff35b8c3151fe3fb453ea14c6509
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2023-12-16 15:37:42 +0700

    add trim to string body

--- app/validators/comment-validator.js
+++ app/validators/comment-validator.js
@@ -3,14 +3,14 @@ const Joi = require('joi')
 const CommentValidator = {
   createComment: (payload) => {
     const schema = Joi.object({
-      comment: Joi.string().required(),
+      comment: Joi.string().trim().required(),
     }).options({ abortEarly: false })
 
     return schema.validate(payload)
   },
   updateComment: (payload) => {
     const schema = Joi.object({
-      comment: Joi.string().required(),
+      comment: Joi.string().trim().required(),
     }).options({ abortEarly: false })
 
     return schema.validate(payload)
